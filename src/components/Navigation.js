32aa57fa5cf07e1b20741fd737cb04a8b781ec85
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-20 13:55:34 +0700

    add more style to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,19 @@
 import { Navbar, Image, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Classes from './Navigation.module.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navigation = () => {
+	const [showMobileMenu, setShowMobileMenu] = useState(false);
+	const mobileMenuHandler = () => {
+		!showMobileMenu ? setShowMobileMenu(true) : setShowMobileMenu(false);
+	};
 	return (
 		<>
-			<Navbar>
+			<Navbar
+				className={`d-flex justify-content-center ${Classes.navbarContainer}`}
+			>
 				<Navbar.Brand>
 					<Image src=''></Image>
 					<Navbar.Text>Kopiku</Navbar.Text>
@@ -16,8 +24,37 @@ const Navigation = () => {
 						<Button>Search</Button>
 					</Form.Group>
 				</Form>
-				<div className={`${Classes.mobileMenu}`}></div>
+				<div
+					className={`d-flex flex-column justify-content-between 
+                    ${Classes.mobileMenu}`}
+					onClick={() => {
+						mobileMenuHandler();
+					}}
+				>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
 			</Navbar>
+			<div
+				className={
+					!showMobileMenu
+						? ` d-none `
+						: `d-flex  flex-column align-items-center ${Classes.mobileMenuContent} `
+				}
+			>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+				</ul>
+			</div>
 		</>
 	);
 };
