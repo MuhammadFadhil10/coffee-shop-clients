cadd502097b2c8cf0e963b2bad61ee98b8dd582d
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-20 08:50:13 +0700

    installing reeact bootstrap, adding navigation component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
 	return (
-		<div className='App'>
+		<Container fluid>
 			<h1>Hello from app</h1>
-		</div>
+		</Container>
 	);
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navigation = () => {
+	return (
+		<>
+			<Navbar>kkkk</Navbar>
+		</>
+	);
+};
+
+export default Navigation;
