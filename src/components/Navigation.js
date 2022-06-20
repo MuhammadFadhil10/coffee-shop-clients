aed418befbfe009e85a6c7692cd9fdbe04bd5cf5
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-20 09:23:15 +0700

    set navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,23 @@
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Image, Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Classes from './Navigation.module.css';
 
 const Navigation = () => {
 	return (
 		<>
-			<Navbar>kkkk</Navbar>
+			<Navbar>
+				<Navbar.Brand>
+					<Image src=''></Image>
+					<Navbar.Text>Kopiku</Navbar.Text>
+				</Navbar.Brand>
+				<Form>
+					<Form.Group className={`d-flex`}>
+						<Form.Control type='text'></Form.Control>
+						<Button>Search</Button>
+					</Form.Group>
+				</Form>
+				<div className={`${Classes.mobileMenu}`}></div>
+			</Navbar>
 		</>
 	);
 };
